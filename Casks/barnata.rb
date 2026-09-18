9201d43c6639f1df870fc02874bedfabd96b1a57
Jacky Luong <27376487+jacksluong@users.noreply.github.com>
2026-09-18 00:48:23 -0400

    Update barnata.rb

--- Casks/barnata.rb
+++ Casks/barnata.rb
@@ -1,15 +1,15 @@
 cask "barnata" do
   # Scripts/release.sh in jacksluong/barnata rewrites these two lines
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "50ba28b1eadf3ac56dcd10e4d179440cdf8d4889f38d837d31571cac61f1dabd"
 
   url "https://github.com/jacksluong/barnata/releases/download/v#{version}/Barnata-#{version}.zip"
   name "Barnata"
   desc "Menu bar app that runs and controls kanata"
   homepage "https://github.com/jacksluong/barnata"
 
-  depends_on macos: ">= :sonoma"
   depends_on arch: :arm64
+  depends_on macos: :sonoma
 
   app "Barnata.app"
 
@@ -17,7 +17,7 @@ cask "barnata" do
             quit:      "io.jackyluong.barnata"
 
   zap trash: [
-    "~/.config/barnata",
     "/Library/Logs/Barnata",
+    "~/.config/barnata",
   ]
 end
